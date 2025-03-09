ddda4a05cb27cfd47e83aba3dd420a392a98474c
yeonrose <sinsu0803@naver.com>
2025-03-09 09:34:44 +0900

    Create Cusomer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placehold.co/200x200',
+  'name' : '동빈나',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placehold.co/200x200',
+  'name' : '이순신',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '디자이너'
+},
+{
+  'id' : 3,
+  'image' : 'https://placehold.co/200x200',
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+
+]
 
 class App extends Component{
   render(){
     return (
-      <div className="gray-background">
-          <img src={logo} lat="logo" />
-          <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map( c => {
+            return(
+              <Customer
+                key= {c.id}
+                id = {c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.name}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+
+          })
+        }
+
       </div>
    )
   }
