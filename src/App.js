26ecea0e350e05efd501d0689df33059f4517029
yeonrose <sinsu0803@naver.com>
2025-03-09 11:50:03 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,59 +1,89 @@
-import React, { Component } from 'react';
-import Customer from './components/Customer'
-import './App.css';
+import React from "react";
+import Customer from "./components/Customer";
+import "./App.css";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { makeStyles } from "@mui/styles";
+
+const theme = createTheme(); // MUI v5 테마 생성
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3), // theme.spacing.unit * 3 → theme.spacing(3) 변경
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+}));
 
 const customers = [
   {
-  'id' : 1,
-  'image' : 'https://placehold.co/200x200',
-  'name' : '동빈나',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placehold.co/200x200',
-  'name' : '이순신',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '디자이너'
-},
-{
-  'id' : 3,
-  'image' : 'https://placehold.co/200x200',
-  'name' : '홍길동',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-}
+    id: 1,
+    image: "https://placehold.co/100x100",
+    name: "동빈나",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placehold.co/100x100",
+    name: "이순신",
+    birthday: "961222",
+    gender: "남자",
+    job: "디자이너",
+  },
+  {
+    id: 3,
+    image: "https://placehold.co/100x100",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
-]
+function App() {
+  const classes = useStyles(); // 스타일 적용
 
-class App extends Component{
-  render(){
-    return (
-      <div>
-        {
-          customers.map( c => {
-            return(
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => (
               <Customer
-                key= {c.id}
-                id = {c.id}
+                key={c.id}
+                id={c.id}
                 image={c.image}
                 name={c.name}
-                birthday={c.name}
+                birthday={c.birthday}
                 gender={c.gender}
                 job={c.job}
               />
-            )
-
-          })
-        }
-
-      </div>
-   )
-  }
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
